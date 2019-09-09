df3e4dcf318416d994dff1e119ddb74f5781eaad
white17werewolf <marina97gankova@gmail.com>
2019-09-09 15:03:31 +0700

    Firts push

---
+++ app/src/main/java/com/android/example/roomwordssample/WordRepository.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.example.roomwordssample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.android.example.roomwordssample.Word;
+import com.android.example.roomwordssample.WordDao;
+import com.android.example.roomwordssample.WordRoomDatabase;
+
+import java.util.List;
+
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    public void deleteAll() {
+        new deleteAllWordsAsyncTask(mWordDao).execute();
+    }
+
+    // Need to run off main thread
+    public void deleteWord(Word word) {
+        new deleteWordAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    /**
+     * Delete all words from the database (does not delete the table)
+     */
+    private static class deleteAllWordsAsyncTask extends AsyncTask<Void, Void, Void> {
+        private WordDao mAsyncTaskDao;
+
+        deleteAllWordsAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            mAsyncTaskDao.deleteAll();
+            return null;
+        }
+    }
+
+    /**
+     *  Delete a single word from the database.
+     */
+    private static class deleteWordAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao mAsyncTaskDao;
+
+        deleteWordAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.deleteWord(params[0]);
+            return null;
+        }
+    }
+}
